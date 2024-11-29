1c4f940affdc09cf9b3288fc15971fe475abfa85
jramirez <jhonatanstiven.ramirez@gmail.com>
2024-11-29 14:58:18 -0500

    4

--- src/assets/components/proyects/Proyects.jsx
+++ src/assets/components/proyects/Proyects.jsx
@@ -113,7 +113,7 @@ const Proyects = () => {
     const [toggle, setToggle] = useState('all');
 
     return (
-        <Container id='pryectos'>
+        <Container id='proyectos'>
             <Wrapper>
                 <Title>Proyectos</Title>
                 <Desc>Here are some of my skills on which I have been working on for the past 2 years.
