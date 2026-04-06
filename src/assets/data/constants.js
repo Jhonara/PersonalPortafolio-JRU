c631aa4c4b496c6bfb9c7a6609e7a9b81643144f
Jhonatan Ramírez <83421988+Jhonara@users.noreply.github.com>
2026-04-05 19:36:15 -0500

    Actualizado

--- src/assets/data/constants.js
+++ src/assets/data/constants.js
@@ -1,3 +1,8 @@
+import ProjectCoopV1 from "../images/Project_CoopV1.png";
+import ProjectCoopV2 from "../images/Project_CoopV2.png";
+import ProjectPKM from "../images/Project_PKM.png";
+import ProjectChatBot from "../images/Project_ChatBot.png";
+
 export const Bio = {
     name: "Jhonatan Stiven Ramírez Useche",
     roles: [
@@ -7,10 +12,11 @@ export const Bio = {
       "Desarrollador Backend",
     ],
     description:
-      "Soy una persona responsable, creativa y con habilidades destacadas para el trabajo en equipo. Con un enfoque en el aprendizaje continuo, he adquirido conocimientos tanto de manera autodidacta como a través de cursos especializados. He participado en proyectos de desarrollo frontend y backend, siempre buscando crear soluciones innovadoras y de calidad. Mi objetivo profesional es crecer en el área de desarrollo de software, aportar valor a cada proyecto y seguir mejorando tanto profesional como personalmente. Estoy comprometido con seguir perfeccionando mis habilidades y contribuir al éxito de los equipos y organizaciones con los que colabore, siempre buscando el equilibrio entre el crecimiento profesional y personal.",
+      "Ingeniero de Sistemas y desarrollador Full Stack con experiencia en tecnologías como Java, Spring Boot, Node.js y React. He participado en el desarrollo de aplicaciones web tanto en frontend como en backend, integrando APIs REST y trabajando con bases de datos relacionales y no relacionales. Me enfoco en crear soluciones funcionales, escalables y bien estructuradas, aplicando buenas prácticas de desarrollo. Actualmente busco seguir creciendo profesionalmente, aportar valor en equipos de desarrollo y fortalecer mis habilidades en tecnologías modernas.",
     github: "https://github.com/Jhonara",
     resume:
-      "https://drive.google.com/file/d/1pXqp_a10zXE3mEUocsmzv72J6H3nVc0z/view?usp=drive_link",
+      //"https://drive.google.com/file/d/1pXqp_a10zXE3mEUocsmzv72J6H3nVc0z/view?usp=drive_link",
+      "https://drive.google.com/file/d/1ckXbm2FtEQbFaJb9APp2gN74M-Lih5ak/view",
     linkedin: "https://www.linkedin.com/in/jhonatan-ru/",
     /*
     twitter: "https://twitter.com/",
@@ -18,71 +24,86 @@ export const Bio = {
     facebook: "https://www.facebook.com//",*/
   };
   
-  export const skills = [
-    {
-      title: "Frontend",
-      skills: [
-        {
-          name: "React Js",
-          image:
-            "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
-        },
-        {
-          name: "Angular",
-          image:
-            "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/512px-Angular_full_color_logo.svg.png",
-        },
-        {
-          name: "HTML",
-          image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
-        },
-        {
-          name: "CSS",
-          image:
-            "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
-        },
-        {
-          name: "JavaScript",
-          image:
-            "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
-        },
-        {
-          name: "Bootstrap",
-          image:
-            "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
-        },
-        {
-          name: "Material UI",
-          image:
-            "https://cdn.worldvectorlogo.com/logos/material-ui-1.svg",
-        },
-        {
-          name: "Tailwind",
-          image:
-            "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Tailwind_CSS_Logo.svg/512px-Tailwind_CSS_Logo.svg.png",
-        },
-      ],
-    },
+  export const skills = [  
     {
       title: "Backend",
       skills: [
         {
           name: "Java",
-          image:
-            "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+          image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg",
+        },
+        {
+          name: "Spring Boot",
+          image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/spring/spring-original.svg",
+        },
+        {
+          name: "Maven",
+          image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/maven/maven-original.svg",
+        },
+        {
+          name: "JSON",
+          image: "https://cdn-icons-png.flaticon.com/512/136/136525.png",
+        },
+        {
+          name: "XML",
+          image: "https://cdn-icons-png.flaticon.com/512/136/136526.png",
         },
         {
           name: "Node Js",
-          image: "https://nodejs.org/static/images/logo.svg",
+          image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg",
         },
         {
-          name: "Spring boot",
-          image:
-            "https://upload.wikimedia.org/wikipedia/commons/7/79/Spring_Boot.svg",
+          name: "Express Js",
+          image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg",
         },
         {
           name: "PHP",
-          image: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/PHP-logo.svg/1896px-PHP-logo.svg.png",
+          image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/php/php-original.svg",
+        },
+        {
+          name: "REST API",
+          image: "https://cdn-icons-png.flaticon.com/512/2165/2165004.png",
+        },
+      ],
+    },
+    {
+      title: "Frontend",
+      skills: [
+        {
+          name: "React Js",
+          image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
+        },
+        {
+          name: "Angular",
+          image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/angularjs/angularjs-original.svg",
+        },
+        {
+          name: "HTML",
+          image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg",
+        },
+        {
+          name: "CSS",
+          image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg",
+        },
+        {
+          name: "JavaScript",
+          image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg",
+        },
+        {
+          name: "Bootstrap",
+          image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg",
+        },
+        {
+          name: "Material UI",
+          image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/materialui/materialui-original.svg",
+        },
+        {
+          name: "Tailwind",
+          image: "https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg",
+        },
+        {
+          name: "Vite",
+          image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vitejs/vitejs-original.svg",
         },
       ],
     },
@@ -91,52 +112,64 @@ export const Bio = {
       skills: [
         {
           name: "MySQL",
-          image:
-            "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+          image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg",
         },
         {
-          name: "Postgresql",
-          image: "https://www.postgresql.org/media/img/about/press/elephant.png",
+          name: "SQL",
+          image: "https://cdn-icons-png.flaticon.com/512/2772/2772128.png",
+        },
+        {
+          name: "PostgreSQL",
+          image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg",
         },
         {
           name: "MongoDB",
-          image:
-            "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+          image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg",
         },
       ],
     },
     {
-      title: "Otros",
+      title: "Herramientas",
       skills: [
         {
           name: "Git",
-          image:
-            "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Git_icon.svg/97px-Git_icon.svg.png?20220905010122",
+          image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg",
         },
         {
           name: "GitHub",
-          image:
-            "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c2/GitHub_Invertocat_Logo.svg/180px-GitHub_Invertocat_Logo.svg.png",
-        },
-        {
-          name: "Netlify",
-          image:
-            "https://static-00.iconduck.com/assets.00/netlify-icon-2048x2048-vn9f0x8q.png",
+          image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg",
         },
         {
           name: "Postman",
-          image:
-            "https://iconape.com/wp-content/png_logo_vector/postman.png",
+          image: "https://www.vectorlogo.zone/logos/getpostman/getpostman-icon.svg",
+        },
+        {
+          name: "Docker",
+          image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg",
+        },
+        {
+          name: "Linux",
+          image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linux/linux-original.svg",
+        },
+        {
+          name: "Netlify",
+          image: "https://www.vectorlogo.zone/logos/netlify/netlify-icon.svg",
         },
         {
           name: "Jira",
-          image:
-            "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8a/Jira_Logo.svg/250px-Jira_Logo.svg.png  ",
+          image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jira/jira-original.svg",
         },
         {
           name: "Trello",
-          image:
-            "https://logos-world.net/wp-content/uploads/2021/02/Trello-Emblem.png",
+          image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/trello/trello-plain.svg",
+        },
+        {
+          name: "IntelliJ IDEA",
+          image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/intellij/intellij-original.svg",
+        },
+        {
+          name: "VS Code",
+          image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original.svg",
         },
       ],
     },
@@ -154,11 +187,11 @@ export const Bio = {
         "SQL",
         "PHP",
         "HTML",
-        "Boostrap",
+        "Bootstrap",
         "React",
         "JavaScript",
-        "TypeScript",
-        "Spring boot",
+        "Linux",
+        "Spring Boot",
       ],
     },
     {
@@ -178,8 +211,8 @@ export const Bio = {
     },
     {
       id: 2,
-      img: "https://media.licdn.com/dms/image/v2/D4E0BAQHvqM5-N73Tng/company-logo_200_200/company-logo_200_200/0/1666365063069?e=1745452800&v=beta&t=jXygRblRzrJWIiFJygieomJLeTkzuKHa6nPJ47RhaAc",
-      role: "Auxiliar de sisetemas ",
+      img: "https://media.licdn.com/dms/image/v2/D4E0BAQHvqM5-N73Tng/company-logo_200_200/company-logo_200_200/0/1666365063069?e=2147483647&v=beta&t=e8bAf_dl8-9YwFylXdvMcyO2glbqo_wbRLnHtOf8bpY",
+      role: "Auxiliar de sistemas",
       company: "Zagamotos - Hyunday del pacifico",
       date: "Mayo 2019 - Abril 2020",
       skills: [
@@ -199,8 +232,8 @@ export const Bio = {
       company: "Auxiliar de diseño grafico y piezas publicitarias",
       date: "Septiembre 2018 - Febrero 2019",
       skills: [
-        "Publcidad",
-        "Creación de piezas publciitarias",
+        "Publicidad",
+        "Creación de piezas publicitarias",
         "Creación de logo",
       ],
     },
@@ -216,31 +249,38 @@ export const Bio = {
     },
     {
       id: 1,
-      img: "https://media.licdn.com/dms/image/v2/D4E0BAQFGKskTA-XUJA/company-logo_200_200/company-logo_200_200/0/1721685266737/servicio_nacional_de_aprendizaje_sena__logo?e=1745452800&v=beta&t=_7zTXfu0Qj52PGTsMDyRRkXVznFVvLuMVnh7x-9avhM",
+      img: "https://bogota.gov.co/sites/default/files/inline-images/logosena.png",
       school: "Servicio Nacional de Aprendizaje (SENA)",
       date: "Noviembre 2020 - Noviembre 2022",
       degree: "Tecnólogo en Análisis y Desarrollo de Sistemas de Información (ADSI)",
     },
     {
       id: 2,
-      img: "https://media.licdn.com/dms/image/v2/D4E0BAQFGKskTA-XUJA/company-logo_200_200/company-logo_200_200/0/1721685266737/servicio_nacional_de_aprendizaje_sena__logo?e=1745452800&v=beta&t=_7zTXfu0Qj52PGTsMDyRRkXVznFVvLuMVnh7x-9avhM",
+      img: "https://bogota.gov.co/sites/default/files/inline-images/logosena.png",
       school: "Servicio Nacional de Aprendizaje (SENA)",
       date: "Mayo 2019 - Mayo 2020",
       degree: "Técnico sistemas",
     }, 
     {
       id: 3,
-      img: "https://media.licdn.com/dms/image/v2/D4E0BAQFGKskTA-XUJA/company-logo_200_200/company-logo_200_200/0/1721685266737/servicio_nacional_de_aprendizaje_sena__logo?e=1745452800&v=beta&t=_7zTXfu0Qj52PGTsMDyRRkXVznFVvLuMVnh7x-9avhM",
+      img: "https://bogota.gov.co/sites/default/files/inline-images/logosena.png",
       school: "Servicio Nacional de Aprendizaje (SENA)",
       date: "Junio 2020 - Octubre 2020 ",
       degree: "Redes y Seguridad",
     },
     {
       id: 4,
-      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkmw0PvmlzoGm8UOlYxfuWpr9pLybBK2bx3Q&s",
+      img: "https://play-lh.googleusercontent.com/MZqXSrt_Fu_QCE97UTUHd2MfeultxA2ECtT99xyzIJX-jcPKBpRohaj9CotLRY2fEA",
       school: "Instituto Centro De Sistemas Del Valle",
       date: "Agosto 2017 - Agosto 2018",
       degree: "Técnico Diseño gráfico",
+    },
+    {
+      id: 5,
+      img: "https://bogota.gov.co/sites/default/files/inline-images/logosena.png",
+      school: "Servicio Nacional de Aprendizaje (SENA)",
+      date: "Agosto 2013 - Agosto 2015",
+      degree: "Técnico programación de software",
     },
   ];
   
@@ -252,7 +292,7 @@ export const Bio = {
       description:
         "Desarrollé una aplicación en React que consume la API de la Pokédex de Pokémon para mostrar información detallada de cada Pokémon.",
       image:
-        "https://private-user-images.githubusercontent.com/83421988/405887321-6342dc29-91a5-4a0d-a67d-53f21db8e3e2.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3Mzc2MTEwNjcsIm5iZiI6MTczNzYxMDc2NywicGF0aCI6Ii84MzQyMTk4OC80MDU4ODczMjEtNjM0MmRjMjktOTFhNS00YTBkLWE2N2QtNTNmMjFkYjhlM2UyLnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNTAxMjMlMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjUwMTIzVDA1MzkyN1omWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPTNjM2M4YTNkNDc3NTkwZjA0MTY0NjZmNDI0ZTYxNDRhZTgxYmViODY3ZWE0Y2VkY2ExYjM0Yjk2Y2M5MjBiMTgmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0In0.x19c7ZFsaDlzmn8mdc3KUzelVAXifSF05lQczvjsL4w",
+        ProjectPKM,
       tags: [
         "React",
         "API",
@@ -269,7 +309,7 @@ export const Bio = {
       description:
         "Página diseñada para cooperativa Coompartir, colocando a prueba mis conocimientos previos",
       image:
-        "https://private-user-images.githubusercontent.com/83421988/405890762-49bd04bd-a6f4-4946-a7af-6375784a353c.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3Mzc2MDg4MjQsIm5iZiI6MTczNzYwODUyNCwicGF0aCI6Ii84MzQyMTk4OC80MDU4OTA3NjItNDliZDA0YmQtYTZmNC00OTQ2LWE3YWYtNjM3NTc4NGEzNTNjLnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNTAxMjMlMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjUwMTIzVDA1MDIwNFomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPWZkNjU5ZDg3MmNiNTgyNDQ5ZDlkMWMzMTY5OWNlMDJjM2Q2MmEyN2RhOTIxNWIzZmQ4M2IzZjQ0ODJjNzljZjQmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0In0.IRkAuEQ3pX_kuJgAEExudoY70KVz0uJmjuFu2OjFU50",
+        ProjectCoopV1,  
       tags: ["HTML", "CSS", "Java Script", "Vanilla",],
       category: "web app",
       github: "http://www.cooperativacoompartir.com/index.html",
@@ -279,11 +319,11 @@ export const Bio = {
     {
       id: 1,
       title: "Actualización Página web Coompartir",
-      date: "Noviembre 2024 - Present",
+      date: "Noviembre 2024",
       description:
         "Actualización de la página web de Cooperativa Coompartir con framework",
       image:
-        "https://private-user-images.githubusercontent.com/83421988/405891521-6ddb0a25-7baa-4d75-9228-118889eeb91c.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3Mzc2MDkwODEsIm5iZiI6MTczNzYwODc4MSwicGF0aCI6Ii84MzQyMTk4OC80MDU4OTE1MjEtNmRkYjBhMjUtN2JhYS00ZDc1LTkyMjgtMTE4ODg5ZWViOTFjLnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNTAxMjMlMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjUwMTIzVDA1MDYyMVomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPWQ5MTM1YzFhNTU0NjQ4OTY0NmRhNTg0N2Q5YjU3Y2MwZmEwNmFjOGQwYWM3OTMwY2ViNmY1ZGVmZTgyZmRmODUmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0In0.zTsyC3nn-7A0eohmILOmk7sA9FN_WklctrPDxRehrh8",
+        ProjectCoopV2,
       tags: [
         "React",
         "Typescrip",
@@ -297,11 +337,11 @@ export const Bio = {
     {
       id: 2,
       title: "Chat bot whatsapp",
-      date: "Diciembte 2024 - Presente",
+      date: "Diciembre 2024",
       description:
         "Chat bot realizado con bot-whatsapp implentando menus interactivos para los usuario y facilitar el servicio al cliente.",
       image:
-        "https://private-user-images.githubusercontent.com/83421988/405899112-603c99b6-34c3-40e3-a261-2f99b46bae49.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3Mzc2MTEyNDAsIm5iZiI6MTczNzYxMDk0MCwicGF0aCI6Ii84MzQyMTk4OC80MDU4OTkxMTItNjAzYzk5YjYtMzRjMy00MGUzLWEyNjEtMmY5OWI0NmJhZTQ5LnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNTAxMjMlMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjUwMTIzVDA1NDIyMFomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPWQ1MGRmY2NkMmI0NGI2MWJkZmE3YTM1NWFkNjRjNWFkMjViY2UxNWE0MmVmYzVjMDc4NTBlZTM1MGVlZjQyOWMmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0In0.l9gWaLrlijxdILRYUky2Ra8Gm5IQHgAap1F6R7BUu0M",
+        ProjectChatBot,
       tags: ["Node.js", "bot-whatsapp", "Java Script",],
       category: "web app",
       github: "https://github.com/Jhonara/bot-whatsapp",
